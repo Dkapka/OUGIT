265a10feaeb6d8848dba670aabdcaa8519809402
Dkapka <dksnipr@gmail.com>
2022-01-17 20:57:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OUGIT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OUGIT.dir/main.cpp.obj"
+  "OUGIT.exe"
+  "OUGIT.exe.manifest"
+  "OUGIT.lib"
+  "OUGIT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OUGIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OUGIT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OUGIT.
+# This may be replaced when dependencies are built.
